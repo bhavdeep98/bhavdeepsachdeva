833adad2ff64269da9d95b35b57ff17456e92072
ArshdeepSingh98 <arshdeep.mailme@gmail.com>
2024-05-04 18:10:53 -0700

    update files

---
+++ script.js
@@ -0,0 +1,14 @@
+function loadPage(pageUrl) {
+    console.log("clicked...", pageUrl);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("main-content").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", pageUrl, true);
+    xhttp.send();
+}
+
+// loadPage("research.html");
