d59963704e75b599144aaabb675c8f45ca4f2c05
bhavdeep98 <bhavdeeosachdeva@gmail.com>
2024-05-04 22:22:37 -0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function loadPage(pageUrl) {
-    console.log("clicked...", pageUrl);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
